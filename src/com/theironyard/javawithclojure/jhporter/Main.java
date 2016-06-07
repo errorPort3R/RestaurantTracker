b7434696f518361934767f393af3bed0f84980ce
jeffryporter <jporter7@gmail.com>
2016-06-07 12:01:55 -0400

    Set up all CRUD

--- src/com/theironyard/javawithclojure/jhporter/Main.java
+++ src/com/theironyard/javawithclojure/jhporter/Main.java
@@ -1,25 +1,117 @@
 package com.theironyard.javawithclojure.jhporter;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
 import java.util.HashMap;
 
-public class Main
-{
+public class Main {
     static HashMap<String, User> users = new HashMap<>();
 
-    public static void main(String[] args)
-    {
-	    Spark.init();
+    public static void main(String[] args) {
+        Spark.init();
         Spark.get(
                 "/",
                 (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
                     HashMap m = new HashMap();
-                    return new ModelAndView(m, "login.html");
+                    if (username == null) {
+                        return new ModelAndView(m, "login.html");
+                    } else {
+                        User user = users.get(username);
+                        m.put("restaurants", user.restaurants);
+                        return new ModelAndView(m, "home.html");
+                    }
                 },
                 new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    String name = request.queryParams("username");
+                    String pass = request.queryParams("password");
+                    if (name == null || pass == null) {
+                        throw new Exception("Name or pass not sent");
+                    }
+
+                    User user = users.get(name);
+                    if (user == null) {
+                        user = new User(name, pass);
+                        users.put(name, user);
+                    } else if (!pass.equals(user.name)) {
+                        throw new Exception("Wrong password");
+                    }
+
+                    Session session = request.session();
+                    session.attribute("username", name);
+
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/create-restaurant",
+                ((request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    if (username == null) {
+                        throw new Exception("Not Logged in!");
+                    }
+
+                    String name = request.queryParams("name");
+                    String location = request.queryParams("location");
+                    int rating = Integer.valueOf(request.queryParams("rating"));
+                    String comment = request.queryParams("comment");
+                    if (name == null || location == null || comment == null) {
+                        throw new Exception("Invalid Fields!");
+                    }
+
+                    User user = users.get(username);
+                    if (user == null) {
+                        throw new Exception("User does not exist!");
+                    }
+                    Restaurant r = new Restaurant(name, location, rating, comment);
+                    user.restaurants.add(r);
+
+                    response.redirect("/");
+                    return "";
+                }
+                )
+        );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/delete-restaurant",
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+                    if (username == null) {
+                        throw new Exception("Not Logged in!");
+                    }
+                    int id = Integer.valueOf(request.queryParams("id"));
+                    User user = users.get(username);
+                    if (id<=0 || id - 1 >= user.restaurants.size())
+                    {
+                        throw new Exception("Not a valid restaurant!");
+                    }
+                    user.restaurants.remove(id - 1);
+
+                    response.redirect("/");
+                    return "";
+
+                }
         );
 
     }
--- src/com/theironyard/javawithclojure/jhporter/User.java
+++ src/com/theironyard/javawithclojure/jhporter/User.java
@@ -1,5 +1,7 @@
 package com.theironyard.javawithclojure.jhporter;
 
+import java.util.ArrayList;
+
 /**
  * Created by jeffryporter on 6/7/16.
  */
@@ -7,6 +9,7 @@ public class User
 {
     String name;
     String password;
+    ArrayList<Restaurant> restaurants = new ArrayList<>();
 
     public User(String name, String password) {
         this.name = name;
